23dd36351d11fc91ad254b7a3ce4f127f53f19a3
Philip Svacha <philip.svacha@gmail.com>
2020-09-08 15:13:39 +0200

    initial commit. editor script for fast mesh generation in editor

---
+++ Assets/Scripts/PlanetGenerator/Planet.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Planet : MonoBehaviour
+{
+    public bool AutoUpdate;
+
+    public PlanetGeneratorSettings Settings;
+    public ColorSettings ColorSettings;
+
+    [HideInInspector]
+    public bool GeneralSettingsFoldout;
+    [HideInInspector]
+    public bool ColorSettingsFoldout;
+
+    [HideInInspector]
+    public MeshFilter PlanetMesh;
+
+    public void GeneratePlanet()
+    {
+        Initialize();
+        GenerateMesh();
+        GenerateColours();
+    }
+
+    private void Initialize()
+    {
+        if (PlanetMesh == null)
+        {
+            GameObject meshObj = new GameObject("mesh");
+            meshObj.transform.parent = transform;
+            PlanetMesh = meshObj.AddComponent<MeshFilter>();
+        }
+    }
+
+    void GenerateMesh()
+    {
+        Mesh mesh = new Mesh();
+        mesh.vertices = new Vector3[] { new Vector3(0, 0, 0), new Vector3(0, 0, Settings.PlanetRadius), new Vector3(Settings.PlanetRadius, 0, 0), new Vector3(Settings.PlanetRadius, 0, Settings.PlanetRadius) };
+        mesh.triangles = new int[] { 0, 1, 2, 1, 3, 2 };
+        mesh.RecalculateBounds();
+        mesh.RecalculateNormals();
+        PlanetMesh.sharedMesh = mesh;
+    }
+
+    void GenerateColours()
+    {
+        PlanetMesh.GetComponent<MeshRenderer>().sharedMaterial.color = ColorSettings.PlanetColor;
+    }
+
+    public void OnColorSettingsUpdated()
+    {
+        if (AutoUpdate)
+        {
+            Initialize();
+            GenerateColours();
+        }
+    }
+    public void OnGeneralSettingsUpdated()
+    {
+        if (AutoUpdate)
+        {
+            Initialize();
+            GenerateMesh();
+        }
+    }
+}
---
+++ Assets/Scripts/PlanetGenerator/PlanetEditor.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(Planet))]
+public class PlanetEditor : Editor
+{
+    Planet Planet;
+    Editor GeneralEditor;
+    Editor ColorEditor;
+
+    public override void OnInspectorGUI()
+    {
+        using (var check = new EditorGUI.ChangeCheckScope())
+        {
+            base.OnInspectorGUI();
+            if(check.changed)
+            {
+                Planet.GeneratePlanet();
+            }
+        }
+
+        if(GUILayout.Button("Generate Planet"))
+        {
+            Planet.GeneratePlanet();
+        }
+
+        DrawSettingsEditor(Planet.Settings, Planet.OnGeneralSettingsUpdated, ref Planet.GeneralSettingsFoldout, ref GeneralEditor);
+        DrawSettingsEditor(Planet.ColorSettings, Planet.OnColorSettingsUpdated, ref Planet.ColorSettingsFoldout, ref ColorEditor);
+    }
+
+    void DrawSettingsEditor(Object settings, System.Action onSettingsUpdated, ref bool foldout, ref Editor editor)
+    {
+        if (settings != null)
+        {
+            foldout = EditorGUILayout.InspectorTitlebar(foldout, settings);
+
+            using (var check = new EditorGUI.ChangeCheckScope())
+            {
+                if (foldout)
+                {
+                    CreateCachedEditor(settings, null, ref editor);
+                    editor.OnInspectorGUI();
+
+                    if (check.changed && onSettingsUpdated != null)
+                        onSettingsUpdated();
+                }
+            }
+        }
+    }
+
+    private void OnEnable()
+    {
+        Planet = (Planet)target;
+    }
+}
